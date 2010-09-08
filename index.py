a4d8b71a020fd3fd8bb006aed5436b6aadaec67c
Flávio Ribeiro <email@flavioribeiro.com>
2010-09-07 23:43:49 -0300

    Add 3 sections
    Add challenges, team and contact

--- index.py
+++ index.py
@@ -4,7 +4,19 @@ app = Flask("CobraTeam Website")
 
 @app.route("/")
 def index():
-    return render_template("index.html", hello_msg = "Dorgas Manolo")
+    return render_template("index.html", msg = "Dorgas Manolo")
+
+@app.route("/staff")
+def staff():
+    return render_template("index.html", msg = "Team")
+
+@app.route("/challenges")
+def challenges():
+    return render_template("index.html", msg = "challenges")
+
+@app.route("/contact")
+def contact():
+    return render_template("index.html", msg = "contact")
 
 if __name__ == "__main__":
     app.run(debug=True)
