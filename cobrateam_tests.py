0171e988f37f7c11cbae63a68347f48cfe6b0c94
andrews medina <andrewsmedina@gmail.com>
2010-09-08 00:56:57 -0300

    add test for index controller

---
+++ cobrateam_tests.py
@@ -0,0 +1,16 @@
+import os
+import index
+import unittest
+import tempfile
+
+class CobraTeamTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app = index.app.test_client()
+            
+    def test_index(self):
+        request = self.app.get('/')
+        assert 'Dorgas Manolo' in request.data
+
+if __name__ == '__main__':
+    unittest.main()
