fd1bb949b5fa53df220c3ea27c77bb449912406c
andrews medina <andrewsmedina@gmail.com>
2010-09-16 00:15:19 -0300

    refactoring website

--- cobrateam_tests.py
+++ cobrateam_tests.py
@@ -10,19 +10,7 @@ class CobraTeamTestCase(unittest.TestCase):
             
     def test_index(self):
         request = self.app.get('/')
-        assert 'Dorgas Manolo' in request.data
+        assert 'Projects' in request.data
         
-    def test_staff(self):
-        request= self.app.get('/staff')
-        assert 'Team' in request.data
-        
-    def test_challenges(self):
-        request= self.app.get('/challenges')
-        assert 'challenges' in request.data
-
-    def test_contact(self):
-        request= self.app.get('/contact')
-        assert 'contact' in request.data
-
 if __name__ == '__main__':
     unittest.main()
--- index.py
+++ index.py
@@ -6,17 +6,5 @@ app = Flask("CobraTeam Website")
 def index():
     return render_template("index.html")
 
-@app.route("/staff")
-def staff():
-    return render_template("staff.html")
-
-@app.route("/challenges")
-def challenges():
-    return render_template("challenges.html")
-
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
-
 if __name__ == "__main__":
     app.run(debug=True)
